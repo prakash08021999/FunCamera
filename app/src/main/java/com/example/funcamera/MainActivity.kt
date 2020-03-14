e5845b4951ec9566433c353475788af6be569067
prakash08021999 <rautelanarayan123@gmail.com>
2020-03-15 02:05:02 +0530

    version 1.0>> asking for permission manifest

--- app/src/main/java/com/example/funcamera/MainActivity.kt
+++ app/src/main/java/com/example/funcamera/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        init()
     }
 
     private fun init(){
